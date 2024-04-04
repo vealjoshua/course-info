fd47798550f1bb38e7625a624bfe4d2db52f8ce1
Joshua Veal-Briscoe <josh.veal-briscoe@daugherty.com>
2024-04-04 13:25:02 -0500

    Store data in db

--- course-info-cli/src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ course-info-cli/src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,7 +1,9 @@
 package com.pluralsight.courseinfo.cli;
 
 import com.pluralsight.courseinfo.cli.service.CourseRetrievalService;
+import com.pluralsight.courseinfo.cli.service.CourseStorageService;
 import com.pluralsight.courseinfo.cli.service.PluralsightCourse;
+import com.pluralsight.courseinfo.repository.CourseRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,11 +33,16 @@ public class CourseRetriever {
         Log.info("Retrieving courses for author '{}'", authorId);
 
         CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
+        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        CourseStorageService courseStorageService = new CourseStorageService(courseRepository);
+
         List<PluralsightCourse> coursesToStore = courseRetrievalService.getCoursesFor(authorId)
                 .stream()
                 .filter(not(PluralsightCourse::isRetired))
                 .toList();
 
         Log.info("Retrieved the following {} courses for {}", coursesToStore.size(), coursesToStore);
+        courseStorageService.storePluralsightCourses(coursesToStore);
+        Log.info("Courses successfully stored");
     }
 }
